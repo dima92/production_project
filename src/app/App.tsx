2439a7a38aea32b430078ee425845a119f9ba20e
dima92 <dima-tkachenko@mail.ru>
2024-01-04 00:45:12 +0300

    AppRouter. Конфиг для роутера

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom';
 import './styles/index.scss';
-import {AboutPageAsync} from "../pages/AboutPage/ui/AboutPage.async";
-import {MainPageAsync} from "../pages/MainPage/ui/MainPage.async";
 import {useTheme} from "./providers/ThemeProvider";
-import {classNames} from "../shared/lib/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
@@ -14,12 +13,7 @@ const App = () => {
         <button onClick={toggleTheme}>TOGGLE</button>
         <Link to={'/'}>Главная</Link>
         <Link to={'/about'}>О сайте</Link>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path={'/about'} element={<AboutPageAsync/>}/>
-            <Route path={'/'} element={<MainPageAsync/>}/>
-          </Routes>
-        </Suspense>
+        <AppRouter/>
       </div>
   );
 };
