31f337e35a3b29620a8ce482c40c08b2697a8a43
dima92 <dima-tkachenko@mail.ru>
2024-01-04 14:43:52 +0300

    Несуществующие маршруты. Лоадер для загрузки страниц

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import {PageLoader} from "shared/ui/PageLoader/PageLoader";
 
 const AppRouter = () => (
     <Suspense fallback={<div>Loading...</div>}>
@@ -10,7 +11,7 @@ const AppRouter = () => (
                     key={path}
                     path={path}
                     element={(
-                        <Suspense fallback={<div>Loading...</div>}>
+                        <Suspense fallback={<PageLoader/>}>
                             <div className="page-wrapper">
                                 {element}
                             </div>
