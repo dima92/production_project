fe696bed1df7d6317cff9e288d91549635547e01
dima92 <dima-tkachenko@mail.ru>
2024-01-11 08:25:29 +0300

    Сайдбар. Состояния кнопки. UI Screenshot test report

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,51 +1,91 @@
 import React from 'react';
-import type { Meta, StoryObj } from '@storybook/react';
+import type {Meta, StoryObj} from '@storybook/react';
 
-import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import { Theme } from 'app/providers/ThemeProvider';
-import { Button, ThemeButton } from './Button';
+import {ThemeDecorator} from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import {Theme} from 'app/providers/ThemeProvider';
+import {Button, ButtonSize, ButtonTheme} from './Button';
 
 const meta = {
-    title: 'shared/Button',
-    component: Button,
-    parameters: {
-        layout: 'centered',
-    },
-    tags: ['autodocs'],
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
+  title: 'shared/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: {control: 'color'},
+  },
 } as Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-    args: {
-        children: 'Text',
-    },
+  args: {
+    children: 'Text',
+  },
 };
 
 export const Clear: Story = {
-    args: {
-        children: 'Text',
-        theme: ThemeButton.CLEAR,
-    },
+  args: {
+    children: 'Text',
+    theme: ButtonTheme.CLEAR,
+  },
 };
 
 export const Outline: Story = {
-    args: {
-        children: 'Text',
-        theme: ThemeButton.OUTLINE,
-    },
+  args: {
+    children: 'Text',
+    theme: ButtonTheme.OUTLINE,
+  },
 };
 
 export const OutlineDark: Story = {
-    args: {
-        children: 'Text',
-        theme: ThemeButton.OUTLINE,
-    },
-    decorators: [
-        ThemeDecorator(Theme.DARK),
-    ],
+  args: {
+    children: 'Text',
+    theme: ButtonTheme.OUTLINE,
+  },
+  decorators: [
+    ThemeDecorator(Theme.DARK),
+  ],
+};
+
+export const BackgroundTheme: Story = {
+  args: {
+    children: 'Text',
+    theme: ButtonTheme.BACKGROUND,
+  }
+};
+
+export const BackgroundInverted: Story = {
+  args: {
+    children: 'Text',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+  }
+};
+
+export const Square: Story = {
+  args: {
+    children: '>',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+    square: true
+  }
+};
+
+export const SquareSizeL: Story = {
+  args: {
+    children: '>',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+    square: true,
+    size: ButtonSize.L
+  }
+};
+
+export const SquareXl: Story = {
+  args: {
+    children: '>',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+    square: true,
+    size: ButtonSize.XL
+  }
 };
