781127de48532098df5836d68b2680bba3268d8f
dima92 <dima-tkachenko@mail.ru>
2024-01-13 07:04:16 +0300

    Авторизация. Reducers, slices, async thunk. Custom text МЕТКА

--- .eslintrc.js
+++ .eslintrc.js
@@ -56,7 +56,8 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
-    'no-param-reassign': 'off'
+    'no-param-reassign': 'off',
+    'consistent-return': 'off'
   },
   globals: {
     __IS_DEV__: true,
