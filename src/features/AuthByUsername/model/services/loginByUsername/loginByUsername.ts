a5cb989f37bdd8eb97646cad4ed921a01891dda0
dima92 <dima-tkachenko@mail.ru>
2024-01-18 18:26:35 +0300

    Тестирование фичи authByUsername. TestAsyncThunk

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { User, userActions } from 'entities/User';
@@ -9,23 +8,23 @@ interface LoginByUsernameProps {
   password: string;
 }
 
-export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, {
-  rejectValue: string
-}>(
+export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { rejectValue: string }>(
     'login/loginByUsername',
     async (authData, thunkAPI) => {
-        try {
-            const response = await axios.post<User>('http://localhost:8000/login', authData);
+      try {
+        const response = await axios.post<User>('http://localhost:8000/login', authData);
 
-            if (!response.data) {
-                throw new Error();
-            }
-
-            localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
-            thunkAPI.dispatch(userActions.setAuthData(response.data));
-        } catch (e) {
-            console.log(e);
-            return thunkAPI.rejectWithValue('error');
+        if (!response.data) {
+          throw new Error();
         }
+
+        localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
+        thunkAPI.dispatch(userActions.setAuthData(response.data));
+
+        return response.data;
+      } catch (e) {
+        console.log(e);
+        return thunkAPI.rejectWithValue('error');
+      }
     },
 );
